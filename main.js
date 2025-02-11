2855dbd7679abacc1c741dde8d954f545feaa4d4
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-02-11 16:36:05 -0300

    tie checker WIP

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //board object
 
 const Board = (function () {
+    //create '3x3' board filles will null
     const gameState = Array(9).fill(null)
 
     function logGame () {
-        console.log(gameState)
+        console.log(gameState[0], gameState[1], gameState[2])
+        console.log(gameState[3], gameState[4], gameState[5])
+        console.log(gameState[6], gameState[7], gameState[8])
     }
 
     function assignSymbol(currentPlayer, arrayLocation){
@@ -29,58 +32,65 @@ const Player = (name, symbol) => {
 const GameController = (() => {
     let player1, player2, currentPlayer, gameHasEnded
 
+    //TODO - function to assign players names
     player1 = Player('Leo', 'x')
     player2 = Player('Tay', 'o')
     currentPlayer = player1;
 
+    function changeCurrentPlayer() {
+        return currentPlayer = currentPlayer === player1 ? player2 : player1
+    }
+
     const winningResults = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3 ,6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    //Based on 3x3 board related to array positioning
     //0 1 2
     //3 4 5
-    //6 7 8  
+    //6 7 8
+    
+    function tieChecker(BoardGameState) {
+        for(i = 0; i < BoardGameState.length; i++){
+            if(BoardGameState[i] === null){
+                return false
+            }
+        }
+        return true
+    }
 
     const winConChecker = (BoardGameState, currentPlayer) => { 
         //check winning condition
         for (value of winningResults){
             //descontruct each possible winning condition positioning from reference array
             const [a, b ,c] = value;
+            //check if current player has won
             if (BoardGameState[a] !== null && BoardGameState[a] === BoardGameState[b] && BoardGameState[b] === BoardGameState[c]){
-                //check if symbol matches current player
-                if(currentPlayer.symbol === BoardGameState[a]){
-                    //declare current player winner
-                    console.log(`${currentPlayer.name} won`)
-                } else {
-                    console.log('Tay/P2 has won')
-                }
+                //declare current player winner
+                console.log(`${currentPlayer.name} is the WINNER`)
                 return gameHasEnded = true
             }
         }
         return gameHasEnded = false;  
     }
-    //assigning points
-    //change player
-    function computePlay() {
-        if(gameHasEnded){
-            
+    //TODO - start game function
+
+    //TODO - turn function
+    const runTurn = (boardPosition) => {
+        //current player assigns marker
+        Board.assignSymbol(currentPlayer, boardPosition);
+        //check for tie
+        tieChecker(Board.gameState)
+        //game check winCon
+        winConChecker(Board.gameState, currentPlayer)
+        //if someone win, game ends
+        if (gameHasEnded){
+            console.log(`${currentPlayer.name} has won!`)
+        } else {
+            changeCurrentPlayer(currentPlayer);
         }
+        return Board.logGame()
+
     }
-
-    //start game
-    function initGame() {
-        currentPlayer = player1;
-        gameHasEnded = false
-        Board.resetBoard()
-        //gameloop
-            //player marks board
-        // while(gameHasEnded = false){
-            
-        
-        // }
-            //board checks current state
-            //currentPlayer changes
-        
-    }
-
-
+  
+    return {currentPlayer, winConChecker, changeCurrentPlayer, runTurn}
 })()
 
 
